269186ac394a174fda628f60cd7318d2f2ab7775
Harievashini <harievashini.dk@outlook.com>
2021-07-10 11:49:39 -0700

    Added functionalities for the case when both cards are not equal

--- start_game.py
+++ start_game.py
@@ -2,9 +2,22 @@ from war_card_game import GenerateCardDeck, PlayCards
 from constants import RANKS, SUITS
 
 
+def cards_not_equal(player, total_cards):
+
+    # The player that picked the highest open card, gets both the cards
+    print(
+        "The highest card belongs to {}, {} gets the cards".format(
+            player.name, player.name
+        )
+    )
+    player.add_cards(total_cards)
+
+
 def play_game(card_deck):
     """
     The card deck is divided evenly among the two players.
+    At every round, each player reveals the top card of their deck.
+    The player with the highest card, gets both the cards.
     """
 
     print("\nWelcome to War Card Game\n")
@@ -23,7 +36,33 @@ def play_game(card_deck):
     player1 = PlayCards(player1_deck)
     print("PLAYER 2")
     player2 = PlayCards(player2_deck)
+    game_round = 1
+    print("\n \nRound 1")
+    print("Each player receives 26 cards at the start")
+    game_over = False
 
+    while not game_over:
+        total_cards = []  # cards on the table
+
+        print("{} draws a card".format(player1.name))
+        player1_open_card = player1.draw_card()
+        print("{} draws a card".format(player2.name))
+        player2_open_card = player2.draw_card()
+
+        total_cards.append(player1_open_card)
+        total_cards.append(player2_open_card)
+
+        # Player 1 picked the highest card
+        if RANKS.index(player1_open_card[1]) > RANKS.index(player2_open_card[1]):
+            cards_not_equal(player1, total_cards)
+
+        # Player 2 picked the highest card
+        elif RANKS.index(player1_open_card[1]) < RANKS.index(player2_open_card[1]):
+            cards_not_equal(player2, total_cards)
+
+        # war begins when both the cards have same value
+        else:
+            pass
 
 if __name__ == "__main__":
     cards_generate = GenerateCardDeck()
